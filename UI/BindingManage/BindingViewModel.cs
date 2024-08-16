fd2f9cc176f2cc621e7ddfe837b19e28557242f1
ljs <ljs01@jdetek.co.kr>
2024-08-16 16:27:39 +0900

    binding1

---
+++ UI/BindingManage/BindingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.BindingManage
+{
+    internal partial class BindingViewModel : ObservableObject
+    {
+        [ObservableProperty] DateTime _timeText;
+
+        internal BindingViewModel()
+        {
+            TimeText = DateTime.Now;
+
+            startTick();
+        }
+
+        private async void startTick()
+        {
+            while (true)
+            {
+                await Task.Delay(500);
+                TimeText = DateTime.Now;
+            }
+        }
+    }
+}
