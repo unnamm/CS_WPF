b060c56e012fec7f3da5392dbbcda6c7f550b570
ljs <ljs01@jdetek.co.kr>
2024-08-19 11:16:04 +0900

    converter

---
+++ UI/ConvertManage/BoolToColorConvert.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace UI.ConvertManage
+{
+    internal class BoolToColorConvert : IValueConverter
+    {
+        //viewmodel -> ui
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool)
+                throw new Exception();
+
+            bool data = (bool)value;
+
+            if (data == true)
+            {
+                return Brushes.Green;
+            }
+            else
+            {
+                return Brushes.Red;
+            }
+        }
+
+        //ui -> viewmodel
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UI/ConvertManage/ConvertViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UI.ConvertManage
+{
+    internal partial class ConvertViewModel : ObservableObject
+    {
+        [ObservableProperty] private bool _value = false;
+
+        public ConvertViewModel()
+        {
+            startTick();
+        }
+
+        private async void startTick()
+        {
+            while(true)
+            {
+                await Task.Delay(1000);
+                Value = !Value;
+            }
+        }
+    }
+}
