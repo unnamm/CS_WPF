627ac18c9287c936acc8f1b181c84fe9a9588183
ljs <ljs01@jdetek.co.kr>
2024-08-29 17:41:57 +0900

    style, setter

---
+++ UI/ComboboxManage/ComboboxView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.ComboboxManage
+{
+    /// <summary>
+    /// ComboboxView.xaml
+    /// </summary>
+    public partial class ComboboxView : UserControl
+    {
+        public ComboboxView()
+        {
+            InitializeComponent();
+            DataContext = new ComboboxViewModel();
+        }
+    }
+}
---
+++ UI/ComboboxManage/ComboboxViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace UI.ComboboxManage
+{
+    internal partial class ComboboxViewModel : ObservableObject
+    {
+        public ObservableCollection<string> Items { get; } = [];
+
+        [ObservableProperty] string _selectedItem1 = string.Empty;
+        [ObservableProperty] string _selectedValue1 = string.Empty;
+
+        public ComboboxViewModel()
+        {
+            Items.Add("data1");
+            Items.Add("data2");
+            Items.Add("data3");
+            Items.Add("data4");
+            Items.Add("data5");
+        }
+
+        [RelayCommand]
+        public void Change(string data)
+        {
+            //select combobox
+        }
+    }
+}
