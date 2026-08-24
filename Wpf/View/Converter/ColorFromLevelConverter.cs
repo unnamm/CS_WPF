282925afb01441104374bb771cbba3bfaa1c10f8
이재승 <ljs01@jdtek.co.kr>
2026-08-24 15:46:01 +0900

    log list apply color

---
+++ Wpf/View/Converter/ColorFromLevelConverter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace View.Converter
+{
+    internal class ColorFromLevelConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is LogLevel level)
+            {
+                var color = level switch
+                {
+                    LogLevel.Warning => Brushes.Orange,
+                    LogLevel.Error => Brushes.Red,
+                    LogLevel.Critical => Brushes.OrangeRed,
+                    _ => Brushes.Black
+                };
+                return color;
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
