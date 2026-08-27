877d1e6dbb2c6ef156320051c83397289f3f7a1a
이재승 <ljs01@jdtek.co.kr>
2026-08-27 10:41:40 +0900

    update property

--- Wpf/View/DashboardViewModel.cs
+++ Wpf/View/DashboardViewModel.cs
@@ -1,7 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
-using System.Collections.ObjectModel;
 using Wpf.Ui.Abstractions.Controls;
 
 namespace View
@@ -9,72 +8,66 @@ namespace View
     [INotifyPropertyChanged]
     public partial class DashboardViewModel : NavigationAware
     {
-        public ObservableCollection<Log.LogEntry> LogList { get; set; }
         readonly ILogger _logger;
-        [ObservableProperty][NotifyPropertyChangedFor(nameof(IsLoginMode))] private bool _isSignUpMode;
+        [ObservableProperty][NotifyPropertyChangedFor(nameof(IsLoginMode))] public partial bool IsSignUpMode { get; set; }
         public bool IsLoginMode => !IsSignUpMode;
-        [ObservableProperty] string? _loginId;
-        [ObservableProperty] string? _loginPassword;
-        [ObservableProperty] string? _signUpId;
-        [ObservableProperty] string? _signUpPassword;
-        [ObservableProperty] string? _signUpPasswordConfirm;
-        [ObservableProperty] string? _message;
+        [ObservableProperty] public partial string? LoginId { get; set; }
+        [ObservableProperty] public partial string? LoginPassword { get; set; }
+        [ObservableProperty] public partial string? SignUpId { get; set; }
+        [ObservableProperty] public partial string? SignUpPassword { get; set; }
+        [ObservableProperty] public partial string? SignUpPasswordConfirm { get; set; }
 
-        public DashboardViewModel(Log.ViewLoggerProvider logProvider, ILogger<DashboardViewModel> logger)
+        public DashboardViewModel(ILogger<DashboardViewModel> logger)
         {
-            LogList = logProvider.Logs;
             _logger = logger;
         }
 
         [RelayCommand]
-        private void GoToSignUp()
+        void GoToSignUp()
         {
-            Message = null;
             IsSignUpMode = true;
         }
 
         [RelayCommand]
-        private void GoToLogin()
+        void GoToLogin()
         {
-            Message = null;
             IsSignUpMode = false;
         }
 
         [RelayCommand]
-        private void Login()
+        void Login()
         {
             if (string.IsNullOrWhiteSpace(LoginId))
             {
-                Message = "id id empty";
+                _logger.LogWarning("ID is empty");
                 return;
             }
             if (string.IsNullOrWhiteSpace(LoginPassword))
             {
-                Message = "password id empty";
+                _logger.LogWarning("Password id empty");
                 return;
             }
 
-            Message = string.Empty;
             _logger.LogInformation("login attempt: {id}", LoginId);
         }
 
         [RelayCommand]
-        private void SignUp()
+        void SignUp()
         {
             if (string.IsNullOrWhiteSpace(SignUpId))
             {
-                Message = "id is empty";
+                _logger.LogWarning("ID is empty");
                 return;
             }
             if (string.IsNullOrWhiteSpace(SignUpPassword))
             {
-                Message = "password is empty";
+                _logger.LogWarning("Password id empty");
                 return;
             }
 
             if (SignUpPassword != SignUpPasswordConfirm)
             {
-                Message = "password is not confirm";
+                _logger.LogWarning("password is not confirm");
                 return;
             }
 
@@ -83,21 +76,13 @@ namespace View
             SignUpId = null;
             SignUpPassword = null;
             SignUpPasswordConfirm = null;
-
-            Message = "complete sign up";
             IsSignUpMode = false;
         }
 
         public override void OnNavigatedTo()
         {
             base.OnNavigatedTo();
-            _logger.LogInformation("to DashboardViewModel");
-        }
-
-        public override void OnNavigatedFrom()
-        {
-            base.OnNavigatedFrom();
-            _logger.LogInformation("from DashboardViewModel");
+            // show before login id
         }
     }
 }
