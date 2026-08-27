02790369006f7cf13ef919f5c96d9c6c1403aca9
이재승 <ljs01@jdtek.co.kr>
2026-08-27 09:49:41 +0900

    add login ui

--- Wpf/View/DashboardViewModel.cs
+++ Wpf/View/DashboardViewModel.cs
@@ -1,23 +1,91 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 using System.Collections.ObjectModel;
-using System.Text;
 using Wpf.Ui.Abstractions.Controls;
 
 namespace View
 {
-    public class DashboardViewModel : NavigationAware
+    [INotifyPropertyChanged]
+    public partial class DashboardViewModel : NavigationAware
     {
         public ObservableCollection<Log.LogEntry> LogList { get; set; }
-
         readonly ILogger _logger;
+        [ObservableProperty][NotifyPropertyChangedFor(nameof(IsLoginMode))] private bool _isSignUpMode;
+        public bool IsLoginMode => !IsSignUpMode;
+        [ObservableProperty] string? _loginId;
+        [ObservableProperty] string? _loginPassword;
+        [ObservableProperty] string? _signUpId;
+        [ObservableProperty] string? _signUpPassword;
+        [ObservableProperty] string? _signUpPasswordConfirm;
+        [ObservableProperty] string? _message;
 
         public DashboardViewModel(Log.ViewLoggerProvider logProvider, ILogger<DashboardViewModel> logger)
         {
             LogList = logProvider.Logs;
             _logger = logger;
-            logger.LogInformation("instance DashboardViewModel");
+        }
+
+        [RelayCommand]
+        private void GoToSignUp()
+        {
+            Message = null;
+            IsSignUpMode = true;
+        }
+
+        [RelayCommand]
+        private void GoToLogin()
+        {
+            Message = null;
+            IsSignUpMode = false;
+        }
+
+        [RelayCommand]
+        private void Login()
+        {
+            if (string.IsNullOrWhiteSpace(LoginId))
+            {
+                Message = "id id empty";
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(LoginPassword))
+            {
+                Message = "password id empty";
+                return;
+            }
+
+            Message = string.Empty;
+            _logger.LogInformation("login attempt: {id}", LoginId);
+        }
+
+        [RelayCommand]
+        private void SignUp()
+        {
+            if (string.IsNullOrWhiteSpace(SignUpId))
+            {
+                Message = "id is empty";
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(SignUpPassword))
+            {
+                Message = "password is empty";
+                return;
+            }
+
+            if (SignUpPassword != SignUpPasswordConfirm)
+            {
+                Message = "password is not confirm";
+                return;
+            }
+
+            _logger.LogInformation("sign up: {id}", SignUpId);
+
+            SignUpId = null;
+            SignUpPassword = null;
+            SignUpPasswordConfirm = null;
+
+            Message = "complete sign up";
+            IsSignUpMode = false;
         }
 
         public override void OnNavigatedTo()
@@ -30,20 +98,6 @@ namespace View
         {
             base.OnNavigatedFrom();
             _logger.LogInformation("from DashboardViewModel");
-        }
-
-        public override async Task OnNavigatedToAsync()
-        {
-            await base.OnNavigatedToAsync();
-            await Task.Delay(1000);
-            _logger.LogInformation("to delay DashboardViewModel");
-        }
-
-        public override async Task OnNavigatedFromAsync()
-        {
-            await base.OnNavigatedFromAsync();
-            await Task.Delay(1000);
-            _logger.LogInformation("from delay DashboardViewModel");
         }
     }
 }
