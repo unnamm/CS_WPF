6d853b3bd93da8a4cd70e3e02ecb04f955e97864
이재승 <ljs01@jdtek.co.kr>
2026-08-31 14:54:14 +0900

    comment

--- Wpf/Database/Permission.cs
+++ Wpf/Database/Permission.cs
@@ -1,8 +1,5 @@
 namespace Database
 {
-    /// <summary>
-    /// RolePermissions.PermissionCode에 저장되는 값 (DB에 별도 마스터 테이블 없이 코드로 관리)
-    /// </summary>
     public enum Permission
     {
         EmployeeManage,
