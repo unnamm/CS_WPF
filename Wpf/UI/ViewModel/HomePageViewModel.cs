d22c6ef554a12d6647164eb7cf179ff116adaade
이재승 <ljs01@jdtek.co.kr>
2026-09-22 15:45:28 +0900

    upload image

--- Wpf/UI/ViewModel/HomePageViewModel.cs
+++ Wpf/UI/ViewModel/HomePageViewModel.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace UI.ViewModel
 {
-    public class HomePageViewModel
+    public partial class HomePageViewModel : ObservableObject
     {
-        
+        [ObservableProperty] public partial BitmapImage? Image { get; set; }
+
+        [RelayCommand]
+        void SelectImage()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
+            };
+
+            if (dialog.ShowDialog() != true)
+                return;
+
+            Image = new BitmapImage(new Uri(dialog.FileName));
+        }
     }
 }
